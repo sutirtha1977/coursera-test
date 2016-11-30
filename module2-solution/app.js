b3c719cc9747699d21b3906890f677c66f70c562
sutirtha1977 <sutirtha.c@gmail.com>
2016-11-30 14:42:36 -0500

    test

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -14,7 +14,6 @@
 
     itemToBuy.listToBuy = ShoppingListCheckOffService.getItem(true);
 
-    // itemToBuy.removeItem = function (itemIndex,name,quantity){
     itemToBuy.removeItem = function (itemIndex){
       ShoppingListCheckOffService.addItem(itemToBuy.listToBuy[itemIndex].name,itemToBuy.listToBuy[itemIndex].quantity);
       ShoppingListCheckOffService.removeItem(itemIndex);
