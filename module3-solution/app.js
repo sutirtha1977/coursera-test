94a49759dd9bdbee6ab8e04437696e518e3869fa
sutirtha1977 <sutirtha.c@gmail.com>
2016-12-04 23:43:39 -0500

    changes

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,7 +5,7 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
 .directive('foundItems', FoundItems)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 function FoundItems() {
   var ddo = {
